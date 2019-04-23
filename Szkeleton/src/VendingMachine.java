e157175d6afbc40cc2563728a56efcd4a02ac9a2
Biro Vince <me@birovince.com>
2019-04-23 23:56:57 +0200

    up

--- Szkeleton/src/VendingMachine.java
+++ Szkeleton/src/VendingMachine.java
@@ -10,7 +10,7 @@ public class VendingMachine extends Thing{
 		for(Direction d:Direction.values()) {
 			Tile t2=t.getNeighbour(d);
 			if(t2!=null) Animal a=t2.getAnimal();
-			if(a!=null)
+			if(a!=null && t2!=null)
 				a.hearBeep();
 		}
 	}
