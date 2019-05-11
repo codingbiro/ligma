8e247fb1aa6806475524408567b17c491c6dc5f3
Kevin Nagy <kevin.nagy98@gmail.com>
2019-05-11 12:55:15 +0200

    grafikus probalkozasok

--- Prototipus/src/GameController.java
+++ Prototipus/src/GameController.java
@@ -1,3 +1,12 @@
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
 
 /**
  * @author Kevin
@@ -42,5 +51,27 @@ public class GameController {
 		//view letrehozasa
 		View v = new View();
 		v.drawMap();
+		
+		JFrame obj = new JFrame();
+		obj.setSize(222,147);
+		obj.setBackground(Color.DARK_GRAY);
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.setTitle("Panda Pláza");
+		obj.setLocationRelativeTo(null);
+		
+		Canvas canvas = new Canvas();
+        canvas.setPreferredSize(new Dimension(222,147));
+        obj.add(canvas);
+        BufferedImage img = null;
+		try {
+		    img = ImageIO.read(new File("images/map_default.jpg"));
+		} catch (IOException e) {
+			System.out.println("kep betoltese sikertelen");
+		}
+        canvas.getGraphics().drawImage(img, 0, 0, null);
+		obj.pack();
 	}
+	
 }
--- Prototipus/src/Map.java
+++ Prototipus/src/Map.java
@@ -14,6 +14,31 @@ public class Map {
 		for(int i = 0; i < 40; i++) {
 			tiles.add(new Tile());
 		}
+		/*
+		//1 bejarat
+		Tile t.setThing(new Entrance());
+		//1 kijarat
+		Tile t.th = new Exit();
+		//1 jatekgep
+		Tile t.th = new SlotMachine();
+		//1 csokiautomata
+		Tile t.th = new VendingMachine();
+		//1 fotel
+		Tile t.th = new Armchair();
+		//2 szekreny
+		Tile t.th = new Wardrobe();
+		Tile t.th = new Wardrobe();
+		
+		//2 orangutan
+		Tile t.setAnimal(new Orangutan());
+		Tile t.a = new Orangutan();
+		//5 panda (2 sima, 3 kulonleges)
+		Tile t.a = new Panda();
+		Tile t.a = new Panda();
+		Tile t.a = new AfraidPanda();
+		Tile t.a = new JumperPanda();
+		Tile t.a = new TiredPanda();
+		*/
 		//2 darab weaktile hozzaadasa a tombhoz
 		tiles.add(new WeakTile());
 		tiles.add(new WeakTile());
